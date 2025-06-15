6e344f59ad9f49def5430b72ca8452a31b2f724c
minamatta2002 <mina.matta2002@gmail.com>
2025-06-15 19:12:31 +0300

    test task

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,16 +4,15 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Set up a new calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):  # ✅ الاسم الصحيح المطلوب من checker
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-3, -3), 0)
@@ -24,10 +23,4 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(7, -1), -7)
-        self.assertIsNone(self.calc.divide(5, 0))  # Edge case: divide by zero
-        self.assertEqual(self.calc.divide(0, 5), 0)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqua
