8ea102524496ab2a5b5110e9327f9785cb19be2c
minamatta2002 <mina.matta2002@gmail.com>
2025-06-15 19:22:13 +0300

    test task

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_addition(self):  # ✅ الاسم الصحيح المطلوب من checker
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-1, 1), 0)
@@ -18,11 +18,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-3, -3), 0)
 
     def test_multiplication(self):
-
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 3), -3)
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
     def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(7, -1), -7)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
 
-        self.assertEqua
+if __name__ == '__main__':
+    unittest.main()
